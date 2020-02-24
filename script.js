6dd2b079d1f6b191835b9e7425f17adb838f34ed
eklavyabhatia <eklavyabhatia@gmail.com>
2020-02-24 22:20:42 +0530

    added the code to generate and solve sudoku

--- script.js
+++ script.js
@@ -1,7 +1,96 @@
-var arr = {}
-for (var i = 0; i < 81; i++) {
-    arr[i] = document.getElementById(i);
-    arr[i].innerText = i
+var arr = [[], [], [], [], [], [], [], [], []]
+for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+        arr[i][j] = document.getElementById(i * 9 + j);
+        arr[i][j].innerText = ''
+    }
 }
 
+var board = [[], [], [], [], [], [], [], [], []]
+
+let button = document.getElementById('generate-sudoku')
+let solve = document.getElementById('solve')
+
 console.log(arr)
+function changeBoard(board) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (board[i][j] != 0)
+                arr[i][j].innerText = board[i][j]
+            else
+                arr[i][j].innerText = ''
+        }
+    }
+}
+
+
+button.onclick = function () {
+    var xhrRequest = new XMLHttpRequest()
+    xhrRequest.onload = function () {
+        var response = JSON.parse(xhrRequest.response)
+        board = response.board
+        changeBoard(board)
+    }
+    xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=random')
+    xhrRequest.send()
+}
+
+function isPossible(board, sr, sc, val) {
+    for (var row = 0; row < 9; row++) {
+        if (board[row][sc] == val) {
+            return false;
+        }
+    }
+
+    for (var col = 0; col < 9; col++) {
+        if (board[sr][col] == val) {
+            return false;
+        }
+    }
+
+    var r = sr - sr % 3;
+    var c = sc - sc % 3;
+
+    for (var cr = r; cr < r + 3; cr++) {
+        for (var cc = c; cc < c + 3; cc++) {
+            if(board[cr][cc]==val){
+                return false;
+            }
+        }
+    }
+    return true;
+
+}
+
+function solveSudokuHelper(board, sr, sc) {
+    if (sr == 9) {
+        changeBoard(board);
+        return;
+    }
+    if (sc == 9) {
+        solveSudokuHelper(board, sr + 1, 0)
+        return;
+    }
+
+    if (board[sr][sc] != 0) {
+        solveSudokuHelper(board, sr, sc + 1);
+        return;
+    }
+
+    for (var i = 1; i <= 9; i++) {
+        if (isPossible(board, sr, sc, i)) {
+            board[sr][sc] = i;
+            solveSudokuHelper(board, sr, sc + 1);
+            board[sr][sc] = 0;
+        }
+    }
+}
+
+function solveSudoku(board) {
+    solveSudokuHelper(board, 0, 0)
+}
+
+solve.onclick = function () {
+    solveSudoku(board)
+   
+}
